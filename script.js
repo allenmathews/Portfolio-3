93ac3f0f97e36d6a8cdb9c9c59f23c1396b45204
Allen Mathews <allenjoemathews@gmail.com>
2022-05-11 11:12:33 +1000

    added js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("fantasy")
+favouriteMovieGenre("space")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("blueberry")
 
 // light, dark
-favouriteMode("dark")
+favouriteMode("light")
 
 // sharp, soft, round
 favouriteEdgeStyle("soft")
